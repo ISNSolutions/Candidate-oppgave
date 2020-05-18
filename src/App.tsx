3c47f09c3fc4d11b55e2a53fabb386d747e66d79
mohamed Ahmed Ibrahim Abdrabou <mohamed@isnsolutions.no>
2020-05-18 10:38:09 +0200

    Revert "init"
    This reverts commit 588b96bc9fd607533bf833f0b587e48bdb08e47d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import styles from './App.module.scss';
-import { IShip } from './Models/IShip';
-import { fetchData } from './DataProvider/fetchData';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  const [ships, setShips] = useState<IShip[]>([]);
-
-  useEffect(()=>{
-    fetchData.getShips().then(_=>setShips(_))
-  },[])
-
+function App() {
   return (
-    <div className={styles.apps365Candidate}>
-      <h1> Ships </h1>
-      {ships.map(_=>{
-        return <ShipCard ship={_} />
-      } )}    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-
-const ShipCard = (props:{ ship: IShip})=>{
-  return (
-    <div className={styles.container}>
-      <div className={styles.row}>
-        <div className="ms-Grid-col ms-sm12">
-          <span className={styles.title}>{props.ship.title}</span>
-        </div>
-      </div>
-      <div className={styles.row + " " +styles.subTitle}>
-        <div className={styles.column4}>
-          <div>Code: {props.ship.shipCode}</div>
-        </div >
-        <div className={[styles.column4,styles.description].join(" ")}>
-          <div >Year: {props.ship.dateBuilt}</div>
-        </div >
-        <div className={styles.column4}>
-          <div >Flag: {props.ship.codeFlag}</div>
-        </div >
-      </div >
-    </div >
-  );
-}
--- src/Models/IShip.ts
+++
@@ -1,7 +0,0 @@
-export interface IShip {
-  id: number;
-  title: string;
-  shipCode: number;
-  dateBuilt: number;
-  codeFlag: string;
-}
