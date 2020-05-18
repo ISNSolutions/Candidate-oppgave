3612e440c866789411167cf7ccca95a78c2e3b36
mohamed Ahmed Ibrahim Abdrabou <mohamed@isnsolutions.no>
2020-05-18 10:39:25 +0200

    Revert "Revert "init""
    This reverts commit 3c47f09c3fc4d11b55e2a53fabb386d747e66d79.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styles from './App.module.scss';
+import { IShip } from './Models/IShip';
+import { fetchData } from './DataProvider/fetchData';
 
-function App() {
+const App = () => {
+  const [ships, setShips] = useState<IShip[]>([]);
+
+  useEffect(()=>{
+    fetchData.getShips().then(_=>setShips(_))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.apps365Candidate}>
+      <h1> Ships </h1>
+      {ships.map(_=>{
+        return <ShipCard ship={_} />
+      } )}    
     </div>
   );
 }
 
 export default App;
+
+const ShipCard = (props:{ ship: IShip})=>{
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className="ms-Grid-col ms-sm12">
+          <span className={styles.title}>{props.ship.title}</span>
+        </div>
+      </div>
+      <div className={styles.row + " " +styles.subTitle}>
+        <div className={styles.column4}>
+          <div>Code: {props.ship.shipCode}</div>
+        </div >
+        <div className={[styles.column4,styles.description].join(" ")}>
+          <div >Year: {props.ship.dateBuilt}</div>
+        </div >
+        <div className={styles.column4}>
+          <div >Flag: {props.ship.codeFlag}</div>
+        </div >
+      </div >
+    </div >
+  );
+}
